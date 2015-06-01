f42c22e26b6cdc80c4c48f42a767e967eb0a41c7
eyalellenbogen <eyale@outlook.com>
2015-06-01 08:41:13 +0300

    changes

--- router.init.js
+++ router.init.js
@@ -1,16 +1,9 @@
 module.exports = function (router) {
-
-	function registerBooks() {
-		var books = require('./services/books');
-		router.get('/books', books.get);
-		router.post('/books', books.post);
-		router.delete('/books/:id', books.delete);
-		router.patch('/books/:id', books.patch);
-	}
+	var books = require('./services/books')(router,'/books');
 
 	return {
 		registerAll: function () {
-			registerBooks();
+			books.register();
 		}
 	};
 };
--- services/books.js
+++ services/books.js
@@ -1,14 +1,36 @@
-module.exports = {
-	get: function (req, res) {
+module.exports = function (router, basePath) {
+
+	function logRequest(req) {
+		console.log(req.method + " recieved at " + req.originalUrl);
+	}
+	function get(req, res) {
+		logRequest(req);
 		return res.json({ message: "this is a successful get" });
-	},
-	post: function (req, res) {
+	}
+
+	function post(req, res) {
+		logRequest(req);
 		return res.json({ message: "You just send this json", req_body: req.body });
-	},
-	delete: function (req, res) {
+	}
+
+	function del(req, res) {
+		logRequest(req);
 		return res.json({ deleted: true, book_id: req.params.id });
-	},
-	patch: function (req, res) {
+	}
+
+	function patch(req, res) {
+		logRequest(req);
 		return res.json({ patched: true, book_id: req.params.id, stam: true });
 	}
+
+	function register() {
+		router.get(basePath, get);
+		router.post(basePath, post);
+		router.patch(basePath + "/:id", patch);
+		router.delete(basePath + "/:id", del);
+	}
+
+	return {
+		register: register
+	};
 };
