aa27952a7162185a437ffde100ec1d9d535c0585
eyal453 <eyale@outlook.com>
2015-10-31 20:11:48 +0200

    adding gpio

--- router.init.js
+++ router.init.js
@@ -1,9 +1,11 @@
 module.exports = function (router) {
 	var books = require('./services/books')(router,'/books');
+	var buzzer = require('./services/buzzer')(router,'/buzzer');
 
 	return {
 		registerAll: function () {
 			books.register();
+			buzzer.register();
 		}
 	};
 };
---
+++ services/buzzer.js
@@ -0,0 +1,32 @@
+module.exports = function (router, basePath) {
+
+	var GpioPin = require("gpio-promise");
+
+	function post(req, res) {
+		var action = req.params.action;
+		if (action == "buzz") {
+			// var pin = new GpioPin(4);
+			// pin.open('out').then(pin.high);
+			// setTimeout(function () {
+			// 	pin.low().then(pin.close());
+			// });
+			res.status(200).send("Buzzing door");
+			return;
+		}
+		if (action == "ring") {
+			res.status(200).send("Ringing bell");
+			return;
+		}
+
+		return res.status(400).send("Invalid action");
+	}
+
+
+	function register() {
+		router.post(basePath + "/:action", post);
+	}
+
+	return {
+		register: register
+	};
+};
