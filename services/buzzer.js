60342e45f88aadd13cb481ceb923e54ac2d978aa
eyal453 <eyale@outlook.com>
2015-11-20 20:40:29 +0200

    a

--- services/buzzer.js
+++ services/buzzer.js
@@ -2,53 +2,59 @@ module.exports = function (router, basePath) {
 	
 	//var GpioPin = require("gpio-promise");
 	var gpio = require("gpio");
-	
+	var pin4 = gpio.export(4, {
+		direction: "out",
+		interval: 200,
+		ready: function(){
+			pin4.set(0);
+		}
+	});
+
 	var isBuzzing = false;
-	
+
 	function isPositiveInteger(n) {
 		return 0 === n % (!isNaN(parseFloat(n)) && 0 <= ~~n);
 	}
-	
-	function post(req, res) {
+
+	function buzz(req, res) {
 		var action = req.params.action;
-		var seconds = 2;
-		if (isPositiveInteger(req.params.seconds)) {
-			var secondsVal = req.params.seconds * 1;
-			seconds = secondsVal > 0?secondsVal :seconds;
+		var seconds = 5;
+		if (isBuzzing) {
+			return res.status(400).send("Buzzing already");
 		}
-		
-		if (action == "buzz") {
-			if (isBuzzing) {
-				return res.status(400).send("Buzzing already");
+		isBuzzing = true;
+		var worker;
+		if (action == 'stop') {
+			worker = function () {
+				pin4.set(0);
 			}
-			isBuzzing = true;
-			var pin4 = gpio.export(4, {
-				direction: "out",
-				interval: 200,
-				ready: function () {
-					pin4.set(1);
-					setTimeout(function () {
-						pin4.set(0);
-						isBuzzing = false;
-					}, seconds * 1000)
-				}
-			});
-			res.status(200).send("Buzzing door");
-			return;
+		} else {
+			worker = function () {
+				pin4.set(1);
+				setTimeout(function () {
+					pin4.set(0);
+					isBuzzing = false;
+				}, seconds * 1000)
+			}
 		}
-		if (action == "ring") {
-			res.status(200).send("Ringing bell");
-			return;
-		}
-		
-		return res.status(400).send("Invalid action");
+
+		pin4 = gpio.export(4, {
+			direction: "out",
+			interval: 200,
+			ready: worker
+		});
+		res.status(200).send("Buzzing door");
 	}
-	
-	
+
+	function ring(req, res) {
+		res.status(200).send("ringing");
+	}
+
 	function register() {
-		router.post(basePath + "/:action/:seconds?", post);
+		router.post(basePath + "/buzz/:action", buzz);
+		router.post(basePath + "/ring", ring);
 	}
-	
+
 	return {
 		register: register
 	};
