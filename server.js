3c2a510d9a2851516bb100d49d9327b1b346f20c
Eyal Ellenbogen <eyale@outlook.com>
2017-06-02 11:09:35 +0300

    Update server.js

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(express.static('public'));
 
-var port = process.env.PORT || 8080;
+var port = process.env.PORT || 9060;
 
 var router = express.Router();
 
